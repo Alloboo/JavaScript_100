effb4135d17b443efecf8415488ef15f5e011400
unknown <hye874025@gmail.com>
2023-07-04 22:05:10 +0900

    1권: 2번 문제 완료

--- 1.js
+++ 1.js
@@ -1,5 +1,6 @@
 /* 자바스크립트 100제 1 */
 
+/*
 // 1번 문제
 var nums = [100, 200, 300, 400, 500];
 
@@ -13,3 +14,23 @@ nums.pop();
 nums.pop();
 console.log(nums);
 // 기존 배열에서 수정(제거)만 일어난다.
+*/
+
+// 2번 문제
+// 데이터
+var arr = [200, 100, 300];
+//배열 내장함수 사용하여 코드 채우기
+console.log(arr);
+// 출력 [200, 100, 10000, 300];
+
+// 내 풀이
+arr.pop();
+arr.push(10000);
+arr.push(300);
+console.log(arr);
+
+// 정답
+arr.splice(2, 0, 10000);
+// splice(start, deleteCount, item) 순서대로 변경을 시작할 인덱스, 삭제할 요소의 수, 추가할 요소
+// 배열의 요소를 삭제하거나 새 요소를 추가하여 배열의 내용을 변경함
+console.log(arr);
